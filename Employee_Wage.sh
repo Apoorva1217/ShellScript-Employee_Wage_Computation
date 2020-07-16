30381a637ff5e05c7b7e9271ced90a2319838923
Apoorva1217 <Apoorva1217@gmail.com>
2020-07-16 12:48:34 +0530

    choice for various functions

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -32,3 +32,14 @@ Parttimewage(){
 	echo "Part time Employee Wage is: $PART_TIME"
 }
 Parttimewage
+
+#Switch case
+echo -e "1.Attendance\n2.Daily Wage\n3.Part Time Wage\n4.Exit"
+read CHOICE
+case $CHOICE in
+	"1")Attendance;;
+	"2")Dailywage;;
+	"3")Parttimewage;;
+	"4")exit;;
+	"*")echo "Invalid choice"
+esac
